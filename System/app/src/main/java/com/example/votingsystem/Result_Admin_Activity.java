3ee60d469b52cdbbe76b99174fec2dfe5a15e766
Jaival <saijajaival@gmail.com>
2019-02-20 21:37:01 +0530

    Pages Added

---
+++ System/app/src/main/java/com/example/votingsystem/Result_Admin_Activity.java
@@ -0,0 +1,13 @@
+package com.example.votingsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Result_Admin_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_admin);
+    }
+}
