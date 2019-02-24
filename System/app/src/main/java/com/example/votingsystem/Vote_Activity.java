17ccd2ba70e75677bce285547eac668cf504dca2
kushalvith <43214905+kushalvith@users.noreply.github.com>
2019-02-24 12:22:30 +0530

    Pages Linked

--- System/app/src/main/java/com/example/votingsystem/Vote_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Vote_Activity.java
@@ -1,7 +1,9 @@
 package com.example.votingsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Vote_Activity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Vote_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+    }
+    public void goTomainActivity(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
