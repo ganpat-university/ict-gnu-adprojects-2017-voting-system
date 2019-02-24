4f0f4c17cde31ca9eccdb7b85b04fc9d95a9a2a5
Jaival <saijajaival@gmail.com>
2019-02-24 11:06:02 +0530

    Error Solved

--- System/app/src/main/java/com/example/votingsystem/MainActivity.java
+++ System/app/src/main/java/com/example/votingsystem/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.votingsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goToAdmin(View view)
+    {
+        Intent intent = new Intent(this, Dashboard_Admin_Activity.class);
+        startActivity(intent);
     }
 }
