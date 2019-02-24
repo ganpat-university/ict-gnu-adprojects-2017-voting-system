4f6db2ef1d7bbca07e28c6328a4818731754d701
vanshika shah <shahvanshika61@gmail.com>
2019-02-24 12:21:05 +0530

    Pages Linked

--- System/app/src/main/java/com/example/votingsystem/Dashboard_Admin_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Dashboard_Admin_Activity.java
@@ -1,7 +1,9 @@
 package com.example.votingsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Dashboard_Admin_Activity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class Dashboard_Admin_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_admin);
+    }
+    public void goToCandidateInfo(View view)
+    {
+        Intent intent = new Intent(this, Candidate_Info_Activity.class);
+        startActivity(intent);
+    }
+    public void goVoteView(View view)
+    {
+        Intent intent = new Intent(this, Voting_Admin_Activity.class);
+        startActivity(intent);
+    }
+    public void goToResultAdmin(View view)
+    {
+        Intent intent = new Intent(this, Result_Activity.class);
+        startActivity(intent);
     }
 }
