7d33b87b72f9a1a5a49d5d155527213e4010c3f8
Tagore <tagoreamisha@gmail.com>
2019-03-08 17:20:37 +0530

    Pages Linked

--- System/app/src/main/java/com/example/votingsystem/Candidate_Admin_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Candidate_Admin_Activity.java
@@ -1,7 +1,9 @@
 package com.example.votingsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Candidate_Admin_Activity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Candidate_Admin_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_candidate);
+    }
+    public void goToCandidateEdit(View view) {
+        Intent intent = new Intent(this, Candidate_Edit_Activity.class);
+        startActivity(intent);
     }
 }
