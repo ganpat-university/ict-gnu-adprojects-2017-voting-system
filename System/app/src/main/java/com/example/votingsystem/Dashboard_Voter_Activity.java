83275159b59363e15d945aef857b45d962d2ea84
Tagore <tagoreamisha@gmail.com>
2019-03-20 17:46:14 +0530

    Button Disable Facility Added

--- System/app/src/main/java/com/example/votingsystem/Dashboard_Voter_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Dashboard_Voter_Activity.java
@@ -13,54 +13,71 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import javax.xml.transform.Result;
-
 public class Dashboard_Voter_Activity extends AppCompatActivity {
-    DatabaseReference DataRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference condRef = DataRef.child("condition_result");
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference condRef = rootRef.child("condition_result");
+    DatabaseReference condref = rootRef.child("condition");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_voter);
+        disableVote();
+        disableResult();
     }
 
-    public void goToCandi(View view)
-    {
+    public void goToCandi(View view) {
         Intent intent = new Intent(this, Candidate_Info_Activity.class);
         startActivity(intent);
     }
-    public void goToVote(View view)
-    {
+
+    public void goToVote(View view) {
         Intent intent = new Intent(this, Vote_Activity.class);
         startActivity(intent);
     }
-    public void goToResult(View view)
-    {
+
+    public void goToResult(View view) {
         Intent intent = new Intent(this, Result_Activity.class);
         startActivity(intent);
     }
-    public void disable() {
 
-       final Button Result = findViewById(R.id.showresult);
-       condRef.addValueEventListener(new ValueEventListener() {
-           @Override
-           public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-               String text = dataSnapshot.getValue(String.class);
-               if(text.equals("1")) {
-                   Result.setEnabled(true);
-               }
-                else if(text.equals("0")) {
-                    Result.setEnabled(false);
-               }
-           }
+    public void disableVote() {
+        final Button Vote = findViewById(R.id.vote);
+        condref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                if (text.equals("0")) {
+                    Vote.setEnabled(false);
+                } else if (text.equals("1")) {
+                    Vote.setEnabled(true);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-           @Override
-           public void onCancelled(@NonNull DatabaseError databaseError) {
-
-           }
+            }
         });
     }
 
+    public void disableResult() {
+        final Button Result = findViewById(R.id.showresult);
+        condRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                if (text.equals("1")) {
+                    Result.setEnabled(true);
+                }
+                else if (text.equals("0")) {
+                    Result.setEnabled(false);
+                }
+                }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
--- System/app/src/main/java/com/example/votingsystem/Result_Admin_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Result_Admin_Activity.java
@@ -2,12 +2,39 @@ package com.example.votingsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Result_Admin_Activity extends AppCompatActivity {
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference condRef = rootRef.child("condition_result");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_admin);
+    }
+
+    public void Show(View view) {
+        Button show = findViewById(R.id.button2);
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                condRef.setValue("1");
+            }
+        });
+    }
+    public void End (View view)
+    {
+        Button end = findViewById(R.id.button3);
+        end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                condRef.setValue("0");
+            }
+        });
     }
 }
