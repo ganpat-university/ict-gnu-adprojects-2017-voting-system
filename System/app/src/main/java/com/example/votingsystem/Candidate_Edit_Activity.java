6ad06eba78725eda286130332932cf221ec5580e
Tagore <tagoreamisha@gmail.com>
2019-05-01 22:11:51 +0530

    Files Changed

--- System/app/src/main/java/com/example/votingsystem/Candidate_Edit_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Candidate_Edit_Activity.java
@@ -1,12 +1,244 @@
 package com.example.votingsystem;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Candidate_Edit_Activity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    protected static final String TAG = "Done";
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference condRef = rootRef.child("Candidate");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_candidate_edit);
+        fetchData();
     }
+    public void fetchData() {
+        condRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final EditText n = findViewById(R.id.Candidate_Name);
+                final EditText pn = findViewById(R.id.Party_Name);
+                final EditText d = findViewById(R.id.Description);
+                String text = dataSnapshot.getValue(String.class);
+                if (text.equals("1")) {
+                    DocumentReference user = db.collection("Candidate").document("Candidate 1");
+                    user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task< DocumentSnapshot > task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot doc = task.getResult();
+                                assert doc != null;
+                                String name = doc.get("Name").toString();
+                                String pname = doc.get("Party").toString();
+                                String des = doc.get("Description").toString();
+                                n.setText(name);
+                                pn.setText(pname);
+                                d.setText(des);
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+                        }
+                    });
+                } else if (text.equals("2")) {
+                    DocumentReference user = db.collection("Candidate").document("Candidate 2");
+                    user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task< DocumentSnapshot > task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot doc = task.getResult();
+                                assert doc != null;
+                                String name = doc.get("Name").toString();
+                                String pname = doc.get("Party").toString();
+                                String des = doc.get("Description").toString();
+                                n.setText(name);
+                                pn.setText(pname);
+                                d.setText(des);
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+                        }
+                    });
+                } else if (text.equals("3")) {
+                    DocumentReference user = db.collection("Candidate").document("Candidate 3");
+                    user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task< DocumentSnapshot > task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot doc = task.getResult();
+                                assert doc != null;
+                                String name = doc.get("Name").toString();
+                                String pname = doc.get("Party").toString();
+                                String des = doc.get("Description").toString();
+                                n.setText(name);
+                                pn.setText(pname);
+                                d.setText(des);
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+                        }
+                    });
+                } else if (text.equals("4")) {
+                    DocumentReference user = db.collection("Candidate").document("Candidate 4");
+                    user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task< DocumentSnapshot > task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot doc = task.getResult();
+                                assert doc != null;
+                                String name = doc.get("Name").toString();
+                                String pname = doc.get("Party").toString();
+                                String des = doc.get("Description").toString();
+                                n.setText(name);
+                                pn.setText(pname);
+                                d.setText(des);
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+                        }
+                    });
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void UpdateData(View view) {
+
+        final EditText Name = findViewById(R.id.Candidate_Name);
+        final EditText Party = findViewById(R.id.Party_Name);
+        final EditText Des = findViewById(R.id.Description);
+
+        String name = Name.getText().toString();
+        String party = Party.getText().toString();
+        String des = Des.getText().toString();
+
+
+       final Map<String,Object> datatoUpdate = new HashMap<>();
+        datatoUpdate.put("Name",name);
+        datatoUpdate.put("Party",party);
+        datatoUpdate.put("Description",des);
+
+        condRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                if(text.equals("1")) {
+                    DocumentReference c = db.collection("Candidate").document("Candidate 1");
+                    c.update(datatoUpdate)
+                            .addOnSuccessListener(new OnSuccessListener< Void >() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(Candidate_Edit_Activity.this, "Updated Successfully",
+                                            Toast.LENGTH_SHORT).show();
+                                    condRef.setValue(0);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+
+                        }
+                    });
+                } else if(text.equals("2")) {
+                    DocumentReference c = db.collection("Candidate").document("Candidate 2");
+                    c.update(datatoUpdate)
+                            .addOnSuccessListener(new OnSuccessListener< Void >() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(Candidate_Edit_Activity.this, "Updated Successfully",
+                                            Toast.LENGTH_SHORT).show();
+                                    condRef.setValue(0);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+
+                        }
+                    });
+                } else if(text.equals("3")) {
+                    DocumentReference c = db.collection("Candidate").document("Candidate 3");
+                    c.update(datatoUpdate)
+                            .addOnSuccessListener(new OnSuccessListener< Void >() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(Candidate_Edit_Activity.this, "Updated Successfully",
+                                            Toast.LENGTH_SHORT).show();
+                                    condRef.setValue(0);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+
+                        }
+                    });
+                } else if(text.equals("4")) {
+                    DocumentReference c = db.collection("Candidate").document("Candidate 4");
+                    c.update(datatoUpdate)
+                            .addOnSuccessListener(new OnSuccessListener< Void >() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(Candidate_Edit_Activity.this, "Updated Successfully",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+
+                        }
+                    });
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
 }
--- System/app/src/main/java/com/example/votingsystem/Vote_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Vote_Activity.java
@@ -2,51 +2,221 @@ package com.example.votingsystem;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.RadioButton;
+import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Vote_Activity extends AppCompatActivity {
-  //  DocumentReference demoRef = FirebaseFirestore.getInstance();
-    protected String candidate;
+
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference condRef = rootRef.child("Candidate");
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    final String TAG="Done";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+        canrad();
+        final Button btn = findViewById(R.id.Vote);
+        btn.setEnabled(false);
     }
 
-    public void canrad() {
-        RadioGroup rg = findViewById(R.id.radioGroup2);
-        RadioButton votebtn = findViewById(rg.getCheckedRadioButtonId());
-        candidate = votebtn.getText().toString();
-
+    public void canrad(){
+        final RadioGroup rg = findViewById(R.id.radioGroup2);
+        final Button btn = findViewById(R.id.Vote);
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
-                switch (i) {
-                    case R.id.radioButton2:
-                        candidate = "Candidate 1";
-                        break;
-                    case R.id.radioButton3:
-                        candidate = "Candidate 2";
-                        break;
-                    case R.id.radioButton4:
-                        candidate = "Candidate 3";
-                        break;
-                    case R.id.radioButton5:
-                        candidate = "Candidate 4";
-                        break;
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.candi_1) {
+                    Toast.makeText(Vote_Activity.this, "Candidate 1 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("1");
+                    btn.setEnabled(true);
+
+                } else if (checkedId == R.id.candi_2) {
+                    Toast.makeText(Vote_Activity.this, "Candidate 2 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("2");
+                    btn.setEnabled(true);
+
+                } else if (checkedId == R.id.candi_3) {
+                    Toast.makeText(Vote_Activity.this, "Candidate 3 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("3");
+                    btn.setEnabled(true);
+
+                } else if (checkedId == R.id.candi_4) {
+                    Toast.makeText(Vote_Activity.this, "Candidate 4 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("4");
+                    btn.setEnabled(true);
+
+                }
+                rg.getCheckedRadioButtonId();
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int index = rg.indexOfChild(findViewById(rg.getCheckedRadioButtonId()));
+                if(index==0){
+                    DocumentReference c1 = db.collection("Vote").document("Candidate 1");
+                    c1.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task< DocumentSnapshot > task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot val = task.getResult();
+                                assert val != null;
+                                int mat = Integer.parseInt(val.get("Count").toString());
+                                mat = mat + 1;
+                                String put = String.valueOf(mat);
+                                Map<String,Object> dataToSave = new HashMap<>();
+                                dataToSave.put("Count",put);
+                                db.collection("Vote").document("Candidate 1").set(dataToSave)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "Data Updated");
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error", e);
+                                    }
+                                });
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+                        }
+                    });
+                }
+                else if(index==1){
+                    DocumentReference c1 = db.collection("Vote").document("Candidate 2");
+                    c1.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task< DocumentSnapshot > task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot val = task.getResult();
+                                assert val != null;
+                                int mat = Integer.parseInt(val.get("Count").toString());
+                                mat = mat + 1;
+                                String put = String.valueOf(mat);
+                                Map<String,Object> dataToSave = new HashMap<>();
+                                dataToSave.put("Count",put);
+                                db.collection("Vote").document("Candidate 2").set(dataToSave)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "Data Updated");
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error", e);
+                                    }
+                                });
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+                        }
+                    });
+                }
+                else if(index==2){
+                    DocumentReference c1 = db.collection("Vote").document("Candidate 3");
+                    c1.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task< DocumentSnapshot > task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot val = task.getResult();
+                                assert val != null;
+                                int mat = Integer.parseInt(val.get("Count").toString());
+                                mat = mat + 1;
+                                String put = String.valueOf(mat);
+                                Map<String,Object> dataToSave = new HashMap<>();
+                                dataToSave.put("Count",put);
+                                db.collection("Vote").document("Candidate 3").set(dataToSave)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "Data Updated");
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error", e);
+                                    }
+                                });
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+                        }
+                    });
+                }
+                else if(index==3){
+                    DocumentReference c1 = db.collection("Vote").document("Candidate 4");
+                    c1.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task< DocumentSnapshot > task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot val = task.getResult();
+                                assert val != null;
+                                int mat = Integer.parseInt(val.get("Count").toString());
+                                mat = mat + 1;
+                                String put = String.valueOf(mat);
+                                Map<String,Object> dataToSave = new HashMap<>();
+                                dataToSave.put("Count",put);
+                                db.collection("Vote").document("Candidate 4").set(dataToSave)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "Data Updated");
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error", e);
+                                    }
+                                });
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error", e);
+                        }
+                    });
                 }
             }
         });
     }
 
-    public void gotomainActivity(View view) {
-
-        Intent intent = new Intent(this, MainActivity.class);
+    public void Logout(View view){
+        Intent intent = new Intent(this, Dashboard_Voter_Activity.class);
         startActivity(intent);
     }
 }
