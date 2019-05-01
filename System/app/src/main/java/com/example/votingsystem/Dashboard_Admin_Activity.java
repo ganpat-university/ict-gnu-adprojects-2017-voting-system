941498e04d64babda6bd15d9d0a0fd3c56479ff3
kushalvith <43214905+kushalvith@users.noreply.github.com>
2019-05-01 22:24:23 +0530

    Files Changed

--- System/app/src/main/java/com/example/votingsystem/Dashboard_Admin_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Dashboard_Admin_Activity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Dashboard_Admin_Activity extends AppCompatActivity {
 
     @Override
@@ -27,4 +29,19 @@ public class Dashboard_Admin_Activity extends AppCompatActivity {
         Intent intent = new Intent(this, Result_Admin_Activity.class);
         startActivity(intent);
     }
+
+    public void Logout(View view) {
+        findViewById(R.id.Logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Dashboard_Admin_Activity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
