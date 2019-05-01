ce4872f68077ecfcd4475e82c2cc010518b48d08
Vanshika <shahvanshika61@gmail.com>
2019-05-01 22:22:21 +0530

    Files Changed

--- System/app/src/main/java/com/example/votingsystem/Candidate_Add_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Candidate_Add_Activity.java
@@ -5,56 +5,92 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Candidate_Add_Activity extends AppCompatActivity {
+
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference condRef = rootRef.child("CandidateC");
+
     protected static final String TAG = "Done";
-    public int candy;
+    public int candy = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_candidate);
+
+
+
+        condRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                candy = dataSnapshot.getValue(Integer.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
+
 
     public void send_data(View view)
     {
         final EditText Name = findViewById(R.id.CandidateName);
         final EditText Party = findViewById(R.id.PartyName);
         final EditText Des = findViewById(R.id.Description);
+        final Button addc = findViewById(R.id.AddC);
 
-        String name = Name.getText().toString();
-        String party = Party.getText().toString();
-        String des = Des.getText().toString();
 
-        Map<String,Object> dataToSave = new HashMap<>();
-        dataToSave.put("Name",name);
-        dataToSave.put("Party",party);
-        dataToSave.put("Description",des);
 
-        db.collection("Candidate").document("Candidate "+candy).set(dataToSave)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Log.d(TAG, "Data Fetched");
-                Name.setText("");
-                Party.setText("");
-                Des.setText("");
-                candy = candy +1;
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.w(TAG, "Error", e);
-            }
-        });
+        if(candy<5) {
+            String name = Name.getText().toString();
+            String party = Party.getText().toString();
+            String des = Des.getText().toString();
+
+            Map<String, Object> dataToSave = new HashMap<>();
+            dataToSave.put("Name", name);
+            dataToSave.put("Party", party);
+            dataToSave.put("Description", des);
+
+            db.collection("Candidate").document("Candidate " + candy).set(dataToSave)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "Data Fetched");
+                            Name.setText("");
+                            Party.setText("");
+                            Des.setText("");
+                            candy = candy + 1;
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w(TAG, "Error", e);
+                }
+            });
+            condRef.setValue(candy);
+        }
+        else
+        {
+            addc.setEnabled(false);
+        }
     }
 }
--- System/app/src/main/java/com/example/votingsystem/Candidate_Admin_Activity.java
+++ System/app/src/main/java/com/example/votingsystem/Candidate_Admin_Activity.java
@@ -4,13 +4,54 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Candidate_Admin_Activity extends AppCompatActivity {
+   // FirebaseFirestore db = FirebaseFirestore.getInstance();
+    protected static final String TAG = "Done";
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference condRef = rootRef.child("Candidate");
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_candidate);
+        condRef.setValue(0);
+        canSelect();
+    }
+    public void canSelect() {
+        start = findViewById(R.id.Edit);
+        final RadioGroup rg = findViewById(R.id.radioGroup);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioButton3) {
+                    Toast.makeText(Candidate_Admin_Activity.this, "Candidate 1 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("1");
+
+                } else if (checkedId == R.id.radioButton4) {
+                    Toast.makeText(Candidate_Admin_Activity.this, "Candidate 2 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("2");
+
+                } else if (checkedId == R.id.radioButton5) {
+                    Toast.makeText(Candidate_Admin_Activity.this, "Candidate 3 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("3");
+
+                } else if (checkedId == R.id.radioButton6) {
+
+                    Toast.makeText(Candidate_Admin_Activity.this, "Candidate 4 selected!", Toast.LENGTH_LONG).show();
+                    condRef.setValue("4");
+                }
+                rg.getCheckedRadioButtonId();
+            }
+        });
     }
     public void goToCandidateEdit(View view) {
         Intent intent = new Intent(this, Candidate_Edit_Activity.class);
