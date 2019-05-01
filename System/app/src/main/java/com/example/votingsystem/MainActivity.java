1d7c7c3b79c254d9ee777bbaa319af516aced3d6
Jaival <saijajaival@gmail.com>
2019-05-01 22:17:47 +0530

    Files Changed

--- System/app/src/main/java/com/example/votingsystem/MainActivity.java
+++ System/app/src/main/java/com/example/votingsystem/MainActivity.java
@@ -1,66 +1,60 @@
 package com.example.votingsystem;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    // Write a message to the database
-//    FirebaseDatabase database = FirebaseDatabase.getInstance();
-//    DatabaseReference myRef = database.getReference("message");
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private static final String TAG = "Done";
-    @Override
+
+    private EditText editText;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        myRef.setValue("Hello, World!");
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Amisha");
-        user.put("last", "Tagore");
-        user.put("born", 2000);
 
-// Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
+//        Intent intent = new Intent(this, MainVerification.class);
+//        startActivity(intent);
+
+        editText = findViewById(R.id.EditTextNumber);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String number = editText.getText().toString().trim();
+
+                if (number.isEmpty() || number.length() < 10) {
+                    editText.setError("Valid number is required");
+                    editText.requestFocus();
+                    return;
+                }
+
+                String phoneNumber = "+" + "91" + number;
+
+                Intent intent = new Intent(MainActivity.this, MainVerification.class);
+                intent.putExtra("PhoneNumber", phoneNumber);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void goToAdmin(View view)
-    {
-        Intent intent = new Intent(this, Dashboard_Admin_Activity.class);
+    public void goToAdminLogin(View view) {
+        Intent intent = new Intent(this, MainAdminLogin.class);
         startActivity(intent);
     }
 
-    public void goToVoters(View view)
-    {
-        Intent intent = new Intent(this, Dashboard_Voter_Activity.class);
-        startActivity(intent);
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, Dashboard_Voter_Activity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
     }
 }
